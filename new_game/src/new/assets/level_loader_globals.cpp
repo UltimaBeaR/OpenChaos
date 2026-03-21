236eed5075579d31d6199cee7b144d9031f57941
ultimabear <thebear@yandex.ru>
2026-03-21 18:33:33 +0300

    stage 4 - iteration (auto commit by ai)

---
+++ new_game/src/new/assets/level_loader_globals.cpp
@@ -0,0 +1,12 @@
+#include "assets/level_loader_globals.h"
+
+// uc_orig: local_next_prim_point (fallen/Source/io.cpp)
+UWORD local_next_prim_point = 0;
+// uc_orig: local_next_prim_face4 (fallen/Source/io.cpp)
+UWORD local_next_prim_face4 = 0;
+// uc_orig: local_next_prim_face3 (fallen/Source/io.cpp)
+UWORD local_next_prim_face3 = 0;
+// uc_orig: local_next_prim_object (fallen/Source/io.cpp)
+UWORD local_next_prim_object = 0;
+// uc_orig: local_next_prim_multi_object (fallen/Source/io.cpp)
+UWORD local_next_prim_multi_object = 0;
