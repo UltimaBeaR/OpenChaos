0c68c86902723a19e4a9e01214dc7e486830469f
ultimabear <thebear@yandex.ru>
2026-04-28 19:55:44 +0300

    mac/linux fix after dead code removal

--- new_game/src/ui/frontend/startscr_globals.cpp
+++ new_game/src/ui/frontend/startscr_globals.cpp
@@ -1,4 +1,5 @@
 #include "ui/frontend/startscr_globals.h"
+#include "engine/core/types.h" // _MAX_PATH (not in <stdlib.h> on macOS/Linux)
 
 // uc_orig: STARTSCR_mission (fallen/Source/startscr.cpp)
 // Bridge between the frontend (mission selection) and the level loader (ELEV_load).
